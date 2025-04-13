030e9142268ab36e6e439a6994c8c40687a07b78
Ruby-Mollo <lopez.lrd07@gmail.com>
2025-04-13 18:38:34 -0500

    Actividad 2: Arreglando el Codigo

--- Testing-UCSM-main/Class/isPrime.py
+++ Testing-UCSM-main/Class/isPrime.py
@@ -1,26 +1,54 @@
-# Archivo: is_prime.py
 import math
 
 def isPrime(number):
-    # broken
+    if number == 2:
+        return True
     if number <= 1 or (number % 2) == 0:
         return False
-    for check in range(3, int(math.sqrt(number))):
-        if number * check == 0:  # Aquí está el error, debería ser (number % check) == 0
+    for check in range(3, int(math.sqrt(number)) + 1):
+        if (number % check) == 0:
             return False
     return True
 
-def check(n):
-    print("isPrime(" + str(n) + ") = " + str(isPrime(n)))
+def isPrime2(number):
+    if number == 2:
+        return True
+ 
+    if number <= 1 or (number % 2) == 0:
+        return False
+    for check in range(3, int(math.sqrt(number)) + 1):
+        if (number % check) == 0:
+            return False
+    return True
 
-# Pruebas manuales
-check(1)
-check(2)
-check(3)
-check(4)
-check(5)
-check(20)
-check(21)
-check(22)
-check(23)
-check(24)
+def test():
+    assert isPrime(1) == False
+    assert isPrime(2) == True
+    assert isPrime(3) == True
+    assert isPrime(4) == False
+    assert isPrime(5) == True
+    assert isPrime(20) == False
+    assert isPrime(21) == False
+    assert isPrime(22) == False
+    assert isPrime(23) == True
+    assert isPrime(24) == False
+    
+    assert isPrime(9) == False
+    
+    # Verificamos que isPrime2 funciona
+    assert isPrime2(1) == False
+    assert isPrime2(2) == True
+    assert isPrime2(3) == True
+    assert isPrime2(4) == False
+    assert isPrime2(5) == True
+    assert isPrime2(9) == False  
+    assert isPrime2(20) == False
+    assert isPrime2(21) == False
+    assert isPrime2(22) == False
+    assert isPrime2(23) == True
+    assert isPrime2(24) == False
+
+if __name__ == "__main__":
+    test()
+    print("Todas las pruebas pasaron.")
+
